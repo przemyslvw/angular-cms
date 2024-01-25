e9f53fdfd3d5f46215cf86438a613d475d9b4ba2
przemyslvw <przemyslvw@gmail.com>
2024-01-26 00:00:49 +0100

    routing

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }, { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }, { path: 'post/:id', loadChildren: () => import('./post/post.module').then(m => m.PostModule) }];
+const routes: Routes = [{ path: '', component: AdminComponent, children: [
+  { path: '', redirectTo:'dashboard', pathMatch:'full' },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }, { path: 'post/:id', loadChildren: () => import('./post/post.module').then(m => m.PostModule) }
+] }, ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
