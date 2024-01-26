aa57dbb23e8d024ca6364296d5910a51df1ae629
przemyslvw <przemyslvw@gmail.com>
2024-01-26 11:45:12 +0100

    angular fire add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // new import
 import { AppComponent } from './app.component';
+import { enviroments } from '../environments/environments';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule // new import
+    AppRoutingModule, // new import
+    provideFirebaseApp(() => initializeApp(enviroments.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
