1f0226ba9e2d18fc3e04da966ae70d47d186e2c4
przemyslvw <przemyslvw@gmial.com>
2025-01-29 21:45:56 +0100

    nawigacja i linki do komponentów

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { environment } from '../environments/environment';
 
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
